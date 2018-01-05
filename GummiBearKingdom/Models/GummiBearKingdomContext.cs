b6dd16ee10fa9a7af537a358eb16f7710cf34ed2
Rane Fields <rane.fields@gmail.com>
2018-01-05 10:33:52 -0800

    Add initial template files

---
+++ GummiBearKingdom/Models/GummiBearKingdomContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+    public class GummiBearKingdomContext : DbContext
+    {
+        public GummiBearKingdomContext() {}
+
+		public GummiBearKingdomContext(DbContextOptions<GummiBearKingdomContext> options)
+            : base(options) {}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options.UseMySql(@"Server=localhost;Port=8889;database=gummibearkingdom;uid=root;pwd=root;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
+
+    }
+}
--- GummiBearKingdom/Startup.cs
+++ GummiBearKingdom/Startup.cs
@@ -5,30 +5,51 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using GummiBearKingdom.Models;
 
 namespace GummiBearKingdom
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFrameworkMySql()
+                    .AddDbContext<GummiBearKingdomContext>(
+                        options => options.UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+            loggerFactory.AddConsole();
         }
     }
+
 }
