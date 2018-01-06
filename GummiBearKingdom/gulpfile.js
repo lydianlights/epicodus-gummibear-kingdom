1df4aa8a5401f089f43237ce883f55cae196ffcb
Rane Fields <rane.fields@gmail.com>
2018-01-05 16:27:34 -0800

    Add gulp and sass to project... but it's not working

---
+++ GummiBearKingdom/gulpfile.js
@@ -0,0 +1,30 @@
+﻿﻿let gulp = require('gulp');
+let del = require('del');
+let sass = require("gulp-sass");
+
+
+// ========== Main Build ========== //
+
+gulp.task('build', ['sass'], function() {
+
+});
+
+
+// ========== Sass ========== //
+
+gulp.task('sass', ['sass:clean'], function() {
+  return gulp.src('Styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./wwwroot/styles'));
+});
+
+gulp.task('sass:clean', function() {
+    return del(['./wwwroot/styles/*']);
+});
+
+
+// ========== Watch ========== //
+
+gulp.task('watch', function() {
+    gulp.watch(['./Styles/*.scss'], ['sass']);
+});
