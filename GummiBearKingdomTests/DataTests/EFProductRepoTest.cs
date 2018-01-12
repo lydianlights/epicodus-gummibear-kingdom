4c993e87ec470ff1e3b6e27a07dbd6149b45efed
Rane Fields <rane.fields@gmail.com>
2018-01-12 14:40:49 -0800

    Add class to test EFProductRepo

---
+++ GummiBearKingdomTests/DataTests/EFProductRepoTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GummiBearKingdom.Data;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdomTests.DataTests
+{
+    [TestClass]
+    public class EFProductRepoTest : IDisposable
+    {
+        public void Dispose()
+        {
+
+        }
+    }
+}
