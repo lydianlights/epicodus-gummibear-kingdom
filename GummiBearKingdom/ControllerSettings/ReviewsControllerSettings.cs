4577315bfc2983cef60242cf56335ad09dc30723
Rane Fields <rane.fields@gmail.com>
2018-01-12 19:50:33 -0800

    Add route to form to add review to product

---
+++ GummiBearKingdom/ControllerSettings/ReviewsControllerSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GummiBearKingdom.Data;
+using GummiBearKingdom.Models;
+
+namespace GummiBearKingdom.Controllers
+{
+    public class ReviewsControllerSettings
+    {
+        public IDbRepo<Product> ProductRepo { get; }
+        public IDbRepo<Review> ReviewRepo { get; }
+
+        public ReviewsControllerSettings()
+        {
+            ProductRepo = new EFProductRepo();
+            ReviewRepo = new EFReviewRepo();
+        }
+
+        public ReviewsControllerSettings(IDbRepo<Product> productRepo, IDbRepo<Review> reviewRepo)
+        {
+            ProductRepo = productRepo;
+            ReviewRepo = reviewRepo;
+        }
+    }
+}
---
+++ GummiBearKingdom/Controllers/ReviewsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GummiBearKingdom.Data;
+using GummiBearKingdom.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummiBearKingdom.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private IDbRepo<Product> productRepo;
+        private IDbRepo<Review> reviewRepo;
+
+        public ReviewsController(ReviewsControllerSettings settings = null)
+        {
+            if (settings == null)
+            {
+                settings = new ReviewsControllerSettings();
+            }
+            productRepo = settings.ProductRepo;
+            reviewRepo = settings.ReviewRepo;
+        }
+
+        [HttpGet, Route("/products/{id}/reviews/add")]
+        public IActionResult AddReviewToProduct(int productId)
+        {
+            Product model = productRepo.Data.FirstOrDefault(product => product.Id == productId);
+            return View(model);
+        }
+    }
+}
