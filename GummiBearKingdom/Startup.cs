9a000079332245170c81dcfd339e6c425f27871d
Rane Fields <rane.fields@gmail.com>
2018-01-13 00:49:33 -0800

    Add conditional to use dev exception page

--- GummiBearKingdom/Startup.cs
+++ GummiBearKingdom/Startup.cs
@@ -42,7 +42,10 @@ namespace GummiBearKingdom
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseStaticFiles();
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
